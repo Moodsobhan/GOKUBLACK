ef8052a9d9b13834493bc1db0603ef6b10f6d750
mahi-khan0 <161739658+mahi-khan0@users.noreply.github.com>
2024-03-16 23:19:15 +0600

    Create 00_00jodo.js

---
+++ scripts/cmds/00_00jodo.js
@@ -0,0 +1,77 @@
+const axios = require("axios");
+
+module.exports = {
+  config: {
+    name: 'jodo',
+    aliases: ["sammi"],
+    version: '3.0',
+    author: 'RUBISH',
+    countDown: 0,
+    role: 0,
+    shortDescription: 'AI CHAT',
+    longDescription: {
+      vi: 'Chat with simma',
+      en: 'Chat with simma'
+    },
+    category: 'AI CHAT',
+    guide: {
+      vi: '   {pn} Hi  ',
+        
+      en: '   {pn} Hi '
+      
+    }
+  },
+
+  onReply: async function ({ api, event, handleReply }) {
+    if (event.type == "message_reply") {
+      const reply = event.body.toLowerCase();
+      if (isNaN(reply)) {
+        try {
+          const response = await axios.get(`https://simma-rubish-api.onrender.com/chat?message=${encodeURIComponent(reply)}`);
+          const ok = response.data.response;
+          await api.sendMessage(ok, event.threadID, (error, info) => {
+            global.GoatBot.onReply.set(info.messageID, {
+              commandName: this.config.name,
+              type: 'reply',
+              messageID: info.messageID,
+              author: event.senderID,
+              link: ok
+            });
+          }, event.messageID);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    }
+  },
+
+  onStart: async function ({ api, args, event }) {
+    try {
+      const rubish = args.join(" ").toLowerCase();
+      if (!args[0]) {
+        api.sendMessage(
+          "Hello I'm Simma\n\nHow can I assist you?",
+          event.threadID,
+          event.messageID
+        );
+        return;
+      }
+      if (rubish) {
+        const response = await axios.get(`https://simma-rubish-api.onrender.com/chat?message=${encodeURIComponent(rubish)}`);
+        const mg = response.data.response;
+        await api.sendMessage({ body: mg }, event.threadID, (error, info) => {
+          global.GoatBot.onReply.set(info.messageID, {
+            commandName: this.config.name,
+            type: 'reply',
+            messageID: info.messageID,
+            author: event.senderID,
+            link: mg
+          });
+        }, event.messageID);
+      }
+    } catch (error) {
+      console.error(`Failed to get an answer: ${error.message}`);
+      api.sendMessage(`${error.message}.\nAn error`, event.threadID, event.messageID);
+    }
+  }
+};
