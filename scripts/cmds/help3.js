fdd5caeab395c5c4785505c50cfbaec3709aa30e
mahi-khan0 <161739658+mahi-khan0@users.noreply.github.com>
2024-03-18 21:46:46 +0600

    Update help3.js

--- scripts/cmds/help3.js
+++ scripts/cmds/help3.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "𝐉𝐎𝐃𝐎 𝐁𝐎𝐓🤖";
+const doNotDelete = "➪𝐉𝐎𝐃𝐎✍︎𝐁𝐎𝐓𖤍";
 /** 
 * @author NTKhang
 * @author: do not delete it
