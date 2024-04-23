0f8bf3df6fff68ce970c1684dfd2d9759e5b3294
MOHAMMAD AYAN <166968155+MR-AYAN-CHOWDHURY@users.noreply.github.com>
2024-04-23 20:32:11 +0600

    Update imagine.js

--- scripts/cmds/imagine.js
+++ scripts/cmds/imagine.js
@@ -1,40 +1,34 @@
-const axios = require('axios');
+const axios = require("axios");
 
 module.exports = {
-  config: {
-    name: 'imagine2',
-    version: '1.0',
-    author: 'OtinXSandip',
-    countDown: 0,
+config: {
+    name: "imagine",
+ version: "1.0",
+    author: "MR.AYAN",
+    countDown: 30,
     role: 0,
-    longDescription: {
-      en: 'Text to Image'
-    },
-    category: 'ai',
+    shortDescription: "Generate Images.",
+    longDescription: "Featuring Image Generator AI From Your Prompt.",
+    category: "ai",
     guide: {
-      en: `{pn} prompt`
-    }
+ en: "{p}{n} <Prompt>",
+ }
   },
-
-  onStart: async function ({ message, api, args, event }) {
-    const prompt = args.join(' ');
-    
-    if (!prompt) {
-      return message.reply("Soraty e");
-    }
-    
-   
-    const baseURL = `https://sandipapi.onrender.com/imagine?prompt=${prompt}`;
-    
-    
-    message.reply("✅| Generating please wait.", async (err, info) => {
-      message.reply({ 
-body: `✅`,
-        attachment: await global.utils.getStreamFromURL(baseURL)
-      });
-      let ui = info.messageID;
-      message.unsend(ui);
-      
-    });
-  }
-};
+ onStart: async function ({ api, event, args, message }) {
+ try { 
+ const samir = args.join(' ');
+ const response = `https://img.restfulapi.repl.co/generate-image?prompt=${samir}`;
+ await message.reply('⏳|Generating Image...\nPlease Wait A Moment.');
+ const form = {
+ body: `✅|Here's Your Image..`
+ };
+ form.attachment = [];
+ form.attachment[0] = await
+ global.utils.getStreamFromURL(response);
+ message.reply(form);
+ } catch (error) {
+ console.error(error);
+ message.reply("An error occurred while fetching response");
+ }
+ }
+}; 
