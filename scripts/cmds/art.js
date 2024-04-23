61a0973ecdfdb19c8b602a569d8278772e3a27a1
MOHAMMAD AYAN <166968155+MR-AYAN-CHOWDHURY@users.noreply.github.com>
2024-04-23 08:24:42 +0600

    Update art.js

--- scripts/cmds/art.js
+++ scripts/cmds/art.js
@@ -1,40 +1,40 @@
-const axios = require('axios');
+const axios = require("axios");
 
 module.exports = {
-  config: {
-    name: "art",
-    version: "1.0",
-    author: "Tashrif",
-    countDown: 5,
-    role: 0,
-    longDescription: {
-      en: 'Convert a image to anime'
-    },
-    category: "image",
-    guide: {
-      en: '{pn} reply to an image and choose model 1 - 52'
-    }
-  },
+ config: {
+ name: "art",
+ role: 0,
+ author: "MR.AYAN",
+ countDown: 5,
+ longDescription: "Art images",
+ category: "AI",
+ guide: {
+ en: "${pn} reply to an image with a prompt and choose model 1 - 52"
+ }
+ },
+ onStart: async function ({ message, api, args, event }) {
+ const text = args.join(' ');
+ 
+ if (!event.messageReply || !event.messageReply.attachments || !event.messageReply.attachments[0]) {
+ return message.reply("Image URL is missing.");
+ }
 
-  onStart: async function({ api, event, args }) {
-    const imageLink = event.messageReply?.attachments[0]?.url;
-    const [model] = args.join(" ");
+ const imgurl = encodeURIComponent(event.messageReply.attachments[0].url);
 
-    if (!model || !imageLink) {
-      return api.sendMessage('❌ | Please reply to an image and provide a model...', event.threadID, event.messageID);
-    }
+ const [prompt, model] = text.split('|').map((text) => text.trim());
+ const puti = model || "37";
+ 
+ api.setMessageReaction("⏳", event.messageID, () => {}, true);
+ const lado = `https://sandyapi.otinxsandeep.repl.co/art?imgurl=${imgurl}&prompt=${encodeURIComponent(prompt)}&model=${puti}`;
 
-    const API = `https://api-samir.onrender.com/api/art?model=${model}&imgurl=${encodeURIComponent(imageLink)}`;
-
-    try {
-      api.setMessageReaction("⏳", event.messageID, () => {}, true);
-      const imageStream = await global.utils.getStreamFromURL(API);
-      api.setMessageReaction("✅", event.messageID, () => {}, true);
-      return api.sendMessage({ attachment: imageStream }, event.threadID, event.messageID);
-      
-    } catch (error) {
-      console.log(error);
-      return api.sendMessage('❌ | Failed to generate the image.', event.threadID, event.messageID);
-    }
-  }
-};
+ message.reply("✅| Generating please wait.", async (err, info) => {
+ const attachment = await global.utils.getStreamFromURL(lado);
+ message.reply({
+ attachment: attachment
+ });
+ let ui = info.messageID; 
+ message.unsend(ui);
+ api.setMessageReaction("✅", event.messageID, () => {}, true);
+ });
+ }
+}; 
