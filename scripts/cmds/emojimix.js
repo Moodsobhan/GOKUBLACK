1e4c9f3bf079978174ec0ad82484697f007b0f25
MOHAMMAD AYAN <166968155+MR-AYAN-CHOWDHURY@users.noreply.github.com>
2024-04-24 18:49:42 +0600

    Update emojimix.js

--- scripts/cmds/emojimix.js
+++ scripts/cmds/emojimix.js
@@ -4,7 +4,7 @@ module.exports = {
 	config: {
 		name: "emojimix",
 		version: "1.4",
-		author: "NTKhang",
+		author: "MR.AYAN",
 		countDown: 5,
 		role: 0,
 		description: {
@@ -74,4 +74,4 @@ async function generateEmojimix(emoji1, emoji2) {
 	catch (e) {
 		return null;
 	}
-}
+} 
