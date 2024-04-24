33ba7d44fa050f20246a172d6522b93689c346ea
MOHAMMAD AYAN <166968155+MR-AYAN-CHOWDHURY@users.noreply.github.com>
2024-04-24 09:32:15 +0600

    Update and rename 00_00baby.js to botgc.js

---
+++ scripts/cmds/botgc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	config: {
+		name: "botgc",
+		version: "1.0",
+		author: "MR.AYAN",
+		role: 0,
+		shortDescription: {
+			en: "Adds the user to a specific thread."
+		},
+		longDescription: {
+			en: "Adds the user to a specific thread and sends them a notification message."
+		},
+		category: "System",
+		guide: {
+			en: "Use {p}join to add yourself to the specified thread."
+		}
+	},
+	onStart: async function ({ api, event, args }) {
+		const threadID = "61558522534273"; // ID of the thread to add the user to
+
+		try {
+			await api.addUserToGroup(event.senderID, threadID);
+			api.sendMessage("You have been added to the group chat. Please check your Spam or Message Request folder if you can't find the group chat.", event.senderID);
+		} catch (error) {
+			api.sendMessage("Failed to add you to the group chat.", event.senderID);
+		}
+	}
+}; 
