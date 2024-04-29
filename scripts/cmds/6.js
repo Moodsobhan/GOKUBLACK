82c9453344b7968111f3d14d4a1e00461ffa8fc0
MOHAMMAD AYAN <166968155+MR-AYAN-CHOWDHURY@users.noreply.github.com>
2024-04-29 22:00:29 +0600

    Update and rename support.js to 6.js

---
+++ scripts/cmds/6.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "🥹",
+        version: "1.0",
+        author: "MR.AYAN", //** original author fb I'd : https://m.me/MR.AYAN.2X **//
+        countDown: 5,
+        role: 0,
+        shortDescription: "No Prefix",
+        longDescription: "No Prefix",
+        category: "reply",
+    },
+onStart: async function(){}, 
+onChat: async function({
+    event,
+    message,
+    getLang
+}) {
+    if (event.body && event.body.toLowerCase() == "🥹") return message.reply("kicce babu kando kn-!!😦");
+}
+}; 
