7d77ba98a92af23888e9f8c3ee86615ff463cb49
MOHAMMAD AYAN <166968155+MR-AYAN-CHOWDHURY@users.noreply.github.com>
2024-05-10 07:01:02 +0600

    Update Karaba.js

--- scripts/cmds/Karaba.js
+++ scripts/cmds/Karaba.js
@@ -5,7 +5,7 @@ module.exports = {
   config: {
     name: "karaba",
     version: "1.0",
-    author: "𝐀𝐒𝐈𝐅 𝐱𝟔𝟗",
+    author: " MR.AYAN",
     countDown: 1,
     role: 0,
     shortDescription: "Karaba Image!",
