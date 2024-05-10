fbbcd9ecec660f1f080d64b8b0a01ae5afe55f8f
MOHAMMAD AYAN <166968155+MR-AYAN-CHOWDHURY@users.noreply.github.com>
2024-05-10 12:53:47 +0600

    Update and rename upt.js to oii.js

---
+++ scripts/cmds/oii.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "oii",
+        version: "1.0",
+        author: "MR.AYAN", //** original author fb I'd : https://m.me/MR.AYAN.2X **//
+        countDown: 5,
+        role: 0,
+        shortDescription: "No Prefix",
+        longDescription: "No Prefix",
+        category: "reply",
+    },
+onStart: async function(){}, 
+onChat: async function({
+    event,
+    message,
+    getLang
+}) {
+    if (event.body && event.body.toLowerCase() == "oii") return message.reply("শুনা কি হইছে বলো আমাকে আমি আছি তো-!!🤭🤭");
+}
+}; 
