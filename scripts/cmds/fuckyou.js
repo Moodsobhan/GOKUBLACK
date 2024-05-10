607c0686f6aa6b248dbc3229aeff82f23422a215
MOHAMMAD AYAN <166968155+MR-AYAN-CHOWDHURY@users.noreply.github.com>
2024-05-10 07:11:02 +0600

    Update and rename 0quiz.js to fuckyou.js

---
+++ scripts/cmds/fuckyou.js
@@ -0,0 +1,22 @@
+module.exports = {
+ config: {
+   name: "fuck you",
+   version: "1.0",
+   author: "MR.AYAN",
+   countDown: 5,
+   role: 0,
+   shortDescription: "no prefix",
+   longDescription: "no prefix",
+   category: "no prefix",
+ },
+  
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "fuck") {
+ return message.reply({
+ body: "Fuck you too😼",
+ attachment: await global.utils.getStreamFromURL("https://i.postimg.cc/tJhkqJKH/2fa.gif")
+ });
+ }
+ }
+}
