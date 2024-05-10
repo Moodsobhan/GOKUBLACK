2b7b593ab98a33d92d37bf5a05fb1b91580af668
MOHAMMAD AYAN <166968155+MR-AYAN-CHOWDHURY@users.noreply.github.com>
2024-05-11 03:05:08 +0600

    Update and rename 0item.js to niji.js

---
+++ scripts/cmds/niji.js
@@ -0,0 +1,137 @@
+function _0x52f8(_0x572f09, _0x56690a) {
+    const _0x3945fc = _0x14fe();
+    return _0x52f8 = function (_0x29820e, _0x124fd4) {
+        _0x29820e = _0x29820e - (-0x2337 + 0x19a8 + -0x7 * -0x174);
+        let _0x47f27f = _0x3945fc[_0x29820e];
+        return _0x47f27f;
+    }, _0x52f8(_0x572f09, _0x56690a);
+}
+const _0x485c08 = _0x52f8;
+function _0x14fe() {
+    const _0x37b241 = [
+        'ecker.verc',
+        '15681rRZcGV',
+        '348XLmPZl',
+        'exports',
+        'reaction',
+        'axios',
+        '-ai-api-sh',
+        'ofanity-ch',
+        '✅\x20IMAGE\x20GE',
+        'join',
+        'words.\x20It\x20',
+        '9380518KtRfWV',
+        'NERATED\x20FR',
+        'age',
+        'uddho.onre',
+        'reply',
+        'YOUR\x20PROMP',
+        '\x27t\x20use\x20bad',
+        'niji',
+        'utils',
+        '1.0',
+        'https://ts',
+        'ate\x20explic',
+        'kXVIg',
+        '1966615wokzon',
+        '12701624hPXymA',
+        '8422407uNttPM',
+        'ne?prompt=',
+        'unsend',
+        'el.app/pro',
+        'An\x20error\x20o',
+        'T:\x20',
+        'get',
+        'messageID',
+        'https://pr',
+        'Arfan\x20Mahi',
+        'fane?t=',
+        'Please\x20don',
+        'nder.com/a',
+        'aIRVC',
+        '6084448JXXWvn',
+        'OM\x20NIJI!\x0a\x0a',
+        '24qkaWPP',
+        'will\x20gener',
+        'pi/animagi',
+        'it\x20things!',
+        'ccurred.',
+        'Please\x20wai',
+        'isProfane',
+        'error',
+        'OxczP',
+        'WsUZw',
+        'QtnGu',
+        '1910418kjBwBm',
+        't...⏳',
+        'nijijourne',
+        'Text\x20to\x20Im',
+        '{pn}\x20promp'
+    ];
+    _0x14fe = function () {
+        return _0x37b241;
+    };
+    return _0x14fe();
+}
+(function (_0x5f3883, _0x4324cf) {
+    const _0x1d6de8 = _0x52f8, _0x4f8e13 = _0x5f3883();
+    while (!![]) {
+        try {
+            const _0x5428ec = -parseInt(_0x1d6de8(0xb4)) / (-0x1381 + -0x14 * -0x45 + -0x1a * -0x8b) + parseInt(_0x1d6de8(0xbb)) / (-0x7c6 + 0x9c1 + 0x5 * -0x65) * (-parseInt(_0x1d6de8(0xba)) / (-0xec * 0x8 + -0x236f + 0x2ad2)) + parseInt(_0x1d6de8(0xa7)) / (-0x16b4 + -0x22e5 + -0x2b * -0x157) + parseInt(_0x1d6de8(0xd1)) / (-0x73 * 0x27 + 0x8ed * 0x1 + 0x89d) * (-parseInt(_0x1d6de8(0xa9)) / (0x1 * 0x1c75 + -0x1a76 + 0x5 * -0x65)) + parseInt(_0x1d6de8(0xc4)) / (-0x22da + -0xfbd * -0x1 + 0x1324) + parseInt(_0x1d6de8(0xd2)) / (0x2 * -0x407 + -0x1199 + 0x1 * 0x19af) + parseInt(_0x1d6de8(0xd3)) / (-0x148 * -0x14 + 0xf3e * 0x2 + -0x3813);
+            if (_0x5428ec === _0x4324cf)
+                break;
+            else
+                _0x4f8e13['push'](_0x4f8e13['shift']());
+        } catch (_0x4741d9) {
+            _0x4f8e13['push'](_0x4f8e13['shift']());
+        }
+    }
+}(_0x14fe, -0xdb * 0x3d1 + 0x194cfe + -0x1 * 0x6e823));
+const axios = require(_0x485c08(0xbe)), {getStreamFromURL} = global[_0x485c08(0xcc)];
+module[_0x485c08(0xbc)] = {
+    'config': {
+        'name': _0x485c08(0xcb),
+        'aliases': [_0x485c08(0xb6) + 'y'],
+        'version': _0x485c08(0xcd),
+        'author': _0x485c08(0xa2) + 'm',
+        'countDown': 0x5,
+        'role': 0x0,
+        'longDescription': _0x485c08(0xb7) + _0x485c08(0xc6),
+        'category': 'image',
+        'guide': { 'en': _0x485c08(0xb8) + 't' }
+    },
+    'onStart': async function ({
+        api: _0x5552fa,
+        args: _0x5e7ce0,
+        message: _0x4887e5,
+        event: _0x5e6f17
+    }) {
+        const _0x71971e = _0x485c08, _0x435466 = {
+                'OxczP': _0x71971e(0xa4) + _0x71971e(0xca) + _0x71971e(0xc3) + _0x71971e(0xaa) + _0x71971e(0xcf) + _0x71971e(0xac) + '😡',
+                'QtnGu': function (_0x51ae7f, _0x22305c) {
+                    return _0x51ae7f(_0x22305c);
+                },
+                'WsUZw': _0x71971e(0xae) + _0x71971e(0xb5),
+                'aIRVC': function (_0x1fca26, _0x596a5d) {
+                    return _0x1fca26(_0x596a5d);
+                },
+                'kXVIg': _0x71971e(0x9d) + _0x71971e(0xad)
+            };
+        try {
+            let _0x166f83 = _0x5e7ce0[_0x71971e(0xc2)]('\x20');
+            const _0x2bb032 = _0x71971e(0xa1) + _0x71971e(0xc0) + _0x71971e(0xb9) + _0x71971e(0xd6) + _0x71971e(0xa3) + _0x166f83, {data: _0x5209d3} = await axios[_0x71971e(0x9f)](_0x2bb032);
+            if (_0x5209d3[_0x71971e(0xaf)])
+                _0x4887e5[_0x71971e(0xc8)](_0x435466[_0x71971e(0xb1)]);
+            else {
+                let _0x1fecff = _0x71971e(0xce) + _0x71971e(0xbf) + _0x71971e(0xc7) + _0x71971e(0xa5) + _0x71971e(0xab) + _0x71971e(0xd4) + _0x435466[_0x71971e(0xb3)](encodeURIComponent, _0x166f83);
+                const _0x5f5d73 = await _0x4887e5[_0x71971e(0xc8)](_0x435466[_0x71971e(0xb2)]);
+                _0x4887e5[_0x71971e(0xbd)]('⏳', _0x5e6f17[_0x71971e(0xa0)]), await _0x4887e5[_0x71971e(0xc8)]({
+                    'body': _0x71971e(0xc1) + _0x71971e(0xc5) + _0x71971e(0xa8) + _0x71971e(0xc9) + _0x71971e(0x9e) + _0x166f83,
+                    'attachment': await _0x435466[_0x71971e(0xa6)](getStreamFromURL, _0x1fecff)
+                }), _0x4887e5[_0x71971e(0xd5)](_0x5f5d73[_0x71971e(0xa0)]), await _0x4887e5[_0x71971e(0xbd)]('✅', _0x5e6f17[_0x71971e(0xa0)]);
+            }
+        } catch (_0x561e5d) {
+            console[_0x71971e(0xb0)](_0x561e5d), _0x4887e5[_0x71971e(0xc8)](_0x435466[_0x71971e(0xd0)]), _0x4887e5[_0x71971e(0xbd)]('❌', _0x5e6f17[_0x71971e(0xa0)]);
+        }
+    }
+};
