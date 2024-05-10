1ea23b71c456700c7ce399b97ca83ea23f089660
MOHAMMAD AYAN <166968155+MR-AYAN-CHOWDHURY@users.noreply.github.com>
2024-05-10 23:37:32 +0600

    Update npx.js

--- scripts/cmds/npx.js
+++ scripts/cmds/npx.js
@@ -1,121 +1,35 @@
-const fs = require("fs");
-const path = require("path");
-
-// Define the path to the alias data file
-const aliasFilePath = "aliases.json";
-
-function saveAliases(aliases) {
-  try {
-    const data = JSON.stringify(aliases);
-    fs.writeFileSync(aliasFilePath, data);
-    console.log("Aliases data saved successfully");
-  } catch (err) {
-    console.error("Error saving aliases data:", err);
-    console.log(`❌ | Error saving aliases data: ${err}`);
-  }
-}
-
-function loadAliases() {
-  try {
-    const data = fs.readFileSync(aliasFilePath);
-    return JSON.parse(data);
-  } catch (err) {
-    console.error("Error loading aliases data:", err);
-    console.log(`❌ | Error loading aliases data: ${err}`);
-    return {};
-  }
-}
-
-module.exports = {
-  config: {
-    name: "npx",
-    version: "1.0",
-    author: "LiANE",
-    role: 2,
-    category: "owner",
-    shortDescription: {
-      en: "Sets a no prefix alias to an existing command without modifying existing files",
-    },
-    longDescription: {
-      en: "Sets a no prefix alias to an existing command without modifying existing files",
-    },
+const axios = require('axios');
+const fs = require('fs-extra');
+module.exports={
+config:{
+  name: "meta",
+    aliases: ["img5"],
+    version: "6.9.0",
+    author: "MR.AYAN",//** api dipto **//
+    countDown: 15,
+    role: 0,
+    shortDescription: "photo genarate",
+    longDescription: "Photo genarate from meta ai",
+    category: "imagination",
     guide: {
-      en: "{pn} add <alias> <command> - Add an alias for a command",
-    },
-  },
-
-  onStart: async function ({ api, args, event, message }) {
-    const subcommand = args[0];
-
-    if (subcommand === "add") {
-      const alias = args[1];
-      const command = args[2];
-
-      if (alias && command) {
-        const aliases = loadAliases();
-
-        if (!aliases[alias]) {
-          // Save the alias and associated command
-          aliases[alias] = command;
-          saveAliases(aliases);
-
-          message.reply(`Alias '${alias}' added for command '${command}'`);
-        } else {
-          message.reply(`Alias '${alias}' already exists for command '${aliases[alias]}'`);
-        }
-      } else {
-        message.reply("Usage: noprefix add <alias> <command>");
-      }
-    } else if (subcommand === "del") {
-      const alias = args[1];
-
-      if (alias) {
-        const aliases = loadAliases();
-
-        if (aliases[alias]) {
-          // Remove the alias
-          delete aliases[alias];
-          saveAliases(aliases);
-
-          message.reply(`Alias '${alias}' has been removed`);
-        } else {
-          message.reply(`Alias '${alias}' does not exist`);
-        }
-      } else {
-        message.reply("Usage: noprefix del <alias>");
-      }
-    } else {
-      message.reply("Usage: {pn} add <alias> <command> OR {pn} del <alias>");
+      en: "{pn} [prompt]"
     }
-  },
-
-  onChat: async function ({ api, args, event, message, role, getLang, usersData, threadsData, dashBoardData }) {
-    const chat = args[0];
-    const commandFile = `${chat}.js`;
-
-    // Check if the file is in aliases.json
-    const aliases = loadAliases();
-    if (!aliases[chat]) {
-      // File not found in aliases.json
-      return;
-    }
-
-    try {
-      const myCommand = require(path.join(__dirname, commandFile));
-
-      // Execute the onStart function of the associated command
-      if (myCommand.onStart && typeof myCommand.onStart === "function") {
-        await myCommand.onStart({ api, args, event, message, role, getLang, usersData, threadsData, dashBoardData });
-      }
-
-      // Execute the onChat function of the associated command
-      if (myCommand.onChat && typeof myCommand.onChat === "function") {
-        await myCommand.onChat ({ api, args, event, message, role, getLang, usersData, threadsData, dashBoardData });
-      }
-    } catch (error) {
-      console.error("Error executing associated command:", error);
-      message.reply(`❌ | Error executing no prefix command!
-${error}`);
-    }
-  },
+},
+onStart:async function ({ args, event, api }) {
+  try {
+    const apiUrl = "http://noobs-api.onrender.com";
+    const prompt = args.join(" ");
+    const wait = await api.sendMessage("Please wait xans <😽", event.threadID);
+    const response = await axios.get(`${apiUrl}/dipto/meta?prompt=${encodeURIComponent(prompt)}&key=dipto008`);
+    const data = response.data.imgUrls;
+     await api.unsendMessage(wait.messageID);
+    await api.sendMessage({
+      body: `✅ | Generated your images`,
+      attachment: await global.utils.getStreamFromURL(data)
+    }, event.threadID ,event.messageID);
+  } catch (e) {
+    console.error(e);
+    await api.sendMessage(`Failed to genarate photo!!!!\rror: ${e.message}`, event.threadID);
+   }
+ }
 };
